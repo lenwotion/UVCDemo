6161532c9cc41def9b3965bd7338a035bf054e46
liangqg111111 <lori_liangqg@aliyun.com>
2018-01-23 11:26:32 +0800

    Initial UVCDemo code

---
+++ app/.externalNativeBuild/cmake/release/x86_64/CMakeFiles/3.6.0-rc2/CMakeASMCompiler.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_ASM_COMPILER "E:/Android/ndk-bundle/toolchains/llvm/prebuilt/windows-x86_64/bin/clang.exe")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "E:/Android/ndk-bundle/toolchains/x86_64-4.9/prebuilt/windows-x86_64/bin/x86_64-linux-android-ar.exe")
+set(CMAKE_RANLIB "E:/Android/ndk-bundle/toolchains/x86_64-4.9/prebuilt/windows-x86_64/bin/x86_64-linux-android-ranlib.exe")
+set(CMAKE_LINKER "E:/Android/ndk-bundle/toolchains/x86_64-4.9/prebuilt/windows-x86_64/bin/x86_64-linux-android-ld.exe")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "Clang")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
